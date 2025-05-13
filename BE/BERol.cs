11a38cd43f9b86c452071d0738b6dd281ce9e4b5
Arlando A. Ifrene <arlandoifrene@gmail.com>
2025-05-13 08:52:19 -0300

    Refactoring in BLL, abstract class to interface. BE definition (props, constructors) BeRol, BeUsuario, BeUsuarioLog...

---
+++ BE/BERol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BE
+{
+    public class BERol
+    {
+        public int IdRol { get; set; }
+        public string NombreRol { get; set; }
+
+        public BERol(string pIdRol, string pNombreRol) 
+        { 
+            IdRol = int.Parse(pIdRol);
+            NombreRol = pNombreRol;
+        
+        }
+
+        public override string ToString()
+        {
+            return NombreRol;
+        }
+    }
+}
---
+++ BE/BEUsuarioLog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BE
+{
+    public class BEUsuarioLog
+    {
+        public int IdLog { get; set; }
+        public BEUsuario Usuario { get; set; }
+        public DateTime FechaHora { get; set; }
+        public string Accion { get; set; }
+
+
+        public BEUsuarioLog(int idLog, BEUsuario pUsuario, DateTime fechaHora, string accion)
+        {
+            IdLog = idLog;
+            Usuario = pUsuario;
+            FechaHora = fechaHora;
+            Accion = accion;
+        }
+
+        public BEUsuarioLog(BEUsuario pUsuario, DateTime fechaHora, string accion)
+        {
+            Usuario = pUsuario;
+            FechaHora = fechaHora;
+            Accion = accion;
+        }
+
+
+
+
+    }
+}
